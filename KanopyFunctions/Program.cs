69cc21ff4a015a0d9035f9b7a292296fe481b734
Stanislav-Forj <DlyaSteam1997@gmail.com>
2023-07-15 22:10:02 +0300

    Убрал лишний код

--- KanopyFunctions/Program.cs
+++ KanopyFunctions/Program.cs
@@ -21,51 +21,6 @@ namespace Test
             var films = new List<Film>();
             var parser = new FilmParser();
             var links = parser.GetLinks("https://kino.mail.ru/cinema/all/");
-            //for (int i = 1; i < 6; i++)
-            //{
-            //    var film = parser.GetFilmFromHtmlData(links[i]);
-            //    films.Add(film);
-            //    var countries = film.Countries.AsEnumerable();
-            //    var addingFilm = new BsonDocument
-            //{
-            //    { "film_id", film.Id },
-            //    { "Название", film.Name },
-            //    { "Год производства", film.PremiereDate },
-            //    { "Страна", new BsonArray
-            //        {
-            //        new BsonDocument{ {"Страна", string.Join(", ", film.Countries) } }
-            //        }
-            //    },
-            //    { "Жанр", new BsonArray
-            //        {
-            //        new BsonDocument{ {"Жанр", string.Join(", ", film.Genres) } }
-            //        }
-            //    },
-            //    { "Режиссер", new BsonArray
-            //        {
-            //        new BsonDocument{ {"Id", string.Join(", ", film.DirectorsId) } }
-            //        }
-            //    },
-            //    { "Актеры", new BsonArray
-            //        {
-            //        new BsonDocument{ {"Id", string.Join(", ", film.ActorsId) } }
-            //        }
-            //    },
-            //    { "Сценарий", new BsonArray
-            //        {
-            //        new BsonDocument{ {"Id", string.Join(", ", film.AuthorsId) } }
-            //        }
-            //    },
-            //    { "Продюсеры", new BsonArray
-            //        {
-            //        new BsonDocument{ {"Id", string.Join(", ", film.ProducersId) } }
-            //        }
-            //    }
-            //};
-            //    collectionFilms.InsertOne(addingFilm);
-            //    Console.WriteLine(i);
-            //}
-
             for (int i = 1; i < 6; i++)
             {
                 var film = parser.GetFilmFromHtmlData(links[i]);
